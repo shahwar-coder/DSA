7f229b82ed82423201cfef10bb7da4b6dd1aea0c
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-29 19:33:55 +0530

    Create 02_Variable_Window_Size_Sliding_Window_Structure.py

---
+++ Sliding_Window/02_Variable_Window_Size_Sliding_Window_Structure.py
@@ -0,0 +1,10 @@
+start = 0
+
+for end in range(len(arr)):
+    # expand window by including arr[end]
+
+    while condition is violated:
+        # shrink window from the left
+        start += 1
+
+    # window [start, end] is currently valid
